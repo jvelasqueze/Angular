55c418d129ea8813264e279a2ab30dff40e2d77e
Jorge Velasquez <jorge.velasqueze@agilethought.com>
2023-03-13 19:31:10 -0600

    fin seccion 3 - diseño

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+ '@angular/router';
+
+import { LoginComponent } from './auth/login/login.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+const router: Routes = [
+  { path: '',
+   component: PagesComponent,
+   children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+   ]
+  },
+  
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+ 
+ 
+ 
+  { path: '**', component: NopagefoundComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( router )
+
+  ],
+exports: [ RouterModule]
+
+})
+export class AppRoutingModule { }
